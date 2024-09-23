205824cf4fc748db95b3dc164782323b71a02411
techplusmi9litevers <kayravehayvanlar6@gmail.com>
2024-09-23 20:30:14 +0300

    Add files via upload
    seçtiğiniz işletim sisteminin tüm sürümlerini sıralar

---
+++ OS_tarihcesi.py
@@ -0,0 +1,168 @@
+def display_android_versions():
+    android_versions = [
+        "1. Android 1.0 (2008)",
+        "2. Android 1.1 (2009)",
+        "3. Android 1.5 Cupcake (2009)",
+        "4. Android 1.6 Donut (2009)",
+        "5. Android 2.0 Eclair (2009)",
+        "6. Android 2.2 Froyo (2010)",
+        "7. Android 2.3 Gingerbread (2010)",
+        "8. Android 3.0 Honeycomb (2011)",
+        "9. Android 4.0 Ice Cream Sandwich (2011)",
+        "10. Android 4.1 Jelly Bean (2012)",
+        "11. Android 4.4 KitKat (2013)",
+        "12. Android 5.0 Lollipop (2014)",
+        "13. Android 6.0 Marshmallow (2015)",
+        "14. Android 7.0 Nougat (2016)",
+        "15. Android 8.0 Oreo (2017)",
+        "16. Android 9.0 Pie (2018)",
+        "17. Android 10 (2019)",
+        "18. Android 11 (2020)",
+        "19. Android 12 (2021)",
+        "20. Android 13 (2022)",
+        "21. Android 14 (2023)",
+        "22. Android 15 (2023)"
+    ]
+    print("\n".join(android_versions))
+
+def display_windows_versions():
+    windows_versions = [
+        "1. Windows 1.0 (1985)",
+        "2. Windows 2.0 (1987)",
+        "3. Windows 3.0 (1990)",
+        "4. Windows 3.1 (1992)",
+        "5. Windows NT 3.1 (1993)",
+        "6. Windows 95 (1995)",
+        "7. Windows NT 4.0 (1996)",
+        "8. Windows 98 (1998)",
+        "9. Windows 98 SE (1999)",
+        "10. Windows Me (2000)",
+        "11. Windows 2000 (2000)",
+        "12. Windows XP (2001)",
+        "13. Windows Vista (2006)",
+        "14. Windows 7 (2009)",
+        "15. Windows 8 (2012)",
+        "16. Windows 8.1 (2013)",
+        "17. Windows 10 (2015)",
+        "18. Windows 11 (2021)"
+    ]
+    print("\n".join(windows_versions))
+
+def display_macos_versions():
+    macos_versions = [
+        "1. Mac OS X 10.0 Cheetah (2001)",
+        "2. Mac OS X 10.1 Puma (2001)",
+        "3. Mac OS X 10.2 Jaguar (2002)",
+        "4. Mac OS X 10.3 Panther (2003)",
+        "5. Mac OS X 10.4 Tiger (2005)",
+        "6. Mac OS X 10.5 Leopard (2007)",
+        "7. Mac OS X 10.6 Snow Leopard (2009)",
+        "8. Mac OS X 10.7 Lion (2011)",
+        "9. Mac OS X 10.8 Mountain Lion (2012)",
+        "10. OS X 10.9 Mavericks (2013)",
+        "11. OS X 10.10 Yosemite (2014)",
+        "12. OS X 10.11 El Capitan (2015)",
+        "13. macOS 10.12 Sierra (2016)",
+        "14. macOS 10.13 High Sierra (2017)",
+        "15. macOS 10.14 Mojave (2018)",
+        "16. macOS 10.15 Catalina (2019)",
+        "17. macOS 11 Big Sur (2020)",
+        "18. macOS 12 Monterey (2021)",
+        "19. macOS 13 Ventura (2022)",
+        "20. macOS 14 Sonoma (2023)"
+    ]
+    print("\n".join(macos_versions))
+
+def display_ubuntu_versions():
+    ubuntu_versions = [
+        "1. Ubuntu 4.10 (Warty Warthog) (2004)",
+        "2. Ubuntu 5.04 (Hoary Hedgehog) (2005)",
+        "3. Ubuntu 5.10 (Breezy Badger) (2005)",
+        "4. Ubuntu 6.06 LTS (Dapper Drake) (2006)",
+        "5. Ubuntu 6.10 (Edgy Eft) (2006)",
+        "6. Ubuntu 7.04 (Feisty Fawn) (2007)",
+        "7. Ubuntu 7.10 (Gutsy Gibbon) (2007)",
+        "8. Ubuntu 8.04 LTS (Hardy Heron) (2008)",
+        "9. Ubuntu 8.10 (Intrepid Ibex) (2008)",
+        "10. Ubuntu 9.04 (Jaunty Jackalope) (2009)",
+        "11. Ubuntu 9.10 (Karmic Koala) (2009)",
+        "12. Ubuntu 10.04 LTS (Lucid Lynx) (2010)",
+        "13. Ubuntu 10.10 (Maverick Meerkat) (2010)",
+        "14. Ubuntu 11.04 (Natty Narwhal) (2011)",
+        "15. Ubuntu 11.10 (Oneiric Ocelot) (2011)",
+        "16. Ubuntu 12.04 LTS (Precise Pangolin) (2012)",
+        "17. Ubuntu 12.10 (Quantal Quetzal) (2012)",
+        "18. Ubuntu 13.04 (Raring Ringtail) (2013)",
+        "19. Ubuntu 13.10 (Saucy Salamander) (2013)",
+        "20. Ubuntu 14.04 LTS (Trusty Tahr) (2014)",
+        "21. Ubuntu 14.10 (Utopic Unicorn) (2014)",
+        "22. Ubuntu 15.04 (Vivid Vervet) (2015)",
+        "23. Ubuntu 15.10 (Wily Werewolf) (2015)",
+        "24. Ubuntu 16.04 LTS (Xenial Xerus) (2016)",
+        "25. Ubuntu 16.10 (Yakkety Yak) (2016)",
+        "26. Ubuntu 17.04 (Zesty Zapus) (2017)",
+        "27. Ubuntu 17.10 (Artful Aardvark) (2017)",
+        "28. Ubuntu 18.04 LTS (Bionic Beaver) (2018)",
+        "29. Ubuntu 18.10 (Cosmic Cuttlefish) (2018)",
+        "30. Ubuntu 19.04 (Disco Dingo) (2019)",
+        "31. Ubuntu 19.10 (Eoan Ermine) (2019)",
+        "32. Ubuntu 20.04 LTS (Focal Fossa) (2020)",
+        "33. Ubuntu 20.10 (Groovy Gorilla) (2020)",
+        "34. Ubuntu 21.04 (Hirsute Hippo) (2021)",
+        "35. Ubuntu 21.10 (Impish Indri) (2021)",
+        "36. Ubuntu 22.04 LTS (Jammy Jellyfish) (2022)",
+        "37. Ubuntu 22.10 (Kinetic Kudu) (2022)",
+        "38. Ubuntu 23.04 (Lunar Lobster) (2023)",
+        "39. Ubuntu 23.10 (Mantic Minotaur) (2023)"
+    ]
+    print("\n".join(ubuntu_versions))
+
+def display_harmonyos_versions():
+    harmonyos_versions = [
+        "1. HarmonyOS 2.0 (2020)",
+        "2. HarmonyOS 2.1 (2021)",
+        "3. HarmonyOS 3.0 (2022)",
+        "4. HarmonyOS 3.1 (2023)"
+    ]
+    print("\n".join(harmonyos_versions))
+
+def display_hyperos_versions():
+    hyperos_versions = [
+        "1. HyperOS 1.0 (2022)",
+        "2. HyperOS 2.0 (2023)"
+    ]
+    print("\n".join(hyperos_versions))
+
+def main():
+    while True:
+        print("Lütfen bir seçenek girin:")
+        print("1. Android sürümleri")
+        print("2. Windows sürümleri")
+        print("3. macOS sürümleri")
+        print("4. Ubuntu sürümleri")
+        print("5. HarmonyOS sürümleri")
+        print("6. HyperOS sürümleri")
+        print("7. Çıkış")
+        
+        choice = input("Seçiminiz (1/2/3/4/5/6/7): ")
+
+        if choice == "1":
+            display_android_versions()
+        elif choice == "2":
+            display_windows_versions()
+        elif choice == "3":
+            display_macos_versions()
+        elif choice == "4":
+            display_ubuntu_versions()
+        elif choice == "5":
+            display_harmonyos_versions()
+        elif choice == "6":
+            display_hyperos_versions()
+        elif choice == "7":
+            print("Çıkış yapılıyor...")
+            break
+        else:
+            print("Geçersiz bir giriş yaptınız. Lütfen tekrar deneyin.")
+
+if __name__ == "__main__":
+    main()
